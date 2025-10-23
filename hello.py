f32c217e01df51baa5e03a4dcda388e0047f7ab6
Linus Eriksson <linus.eriksson.07@gmail.com>
2025-10-23 12:33:14 +0000

    Del 2 är klar

--- hello.py
+++ hello.py
@@ -2,3 +2,10 @@ import random as rand
 slumptal=rand.randint(1,10)
 namn=(input("Hej vad heter du?"))
 print(f"Hej {namn} och välkommen till spelet!")
+
+#Spelet
+Gissning=int(input("Gissa på ett tal mellan 1-10"))
+if Gissning==slumptal:
+    print("Grattis du gissade rätt!!")
+else:
+    print(f"Tyvär du gissade fel, det rätta svaret var {slumptal}")
