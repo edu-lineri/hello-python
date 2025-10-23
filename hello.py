26763092ecb646d80a3ce2a499d80b0894d1c6b0
Linus Eriksson <linus.eriksson.07@gmail.com>
2025-10-23 12:46:41 +0000

    Klar med del 2

--- hello.py
+++ hello.py
@@ -1,10 +1,26 @@
-print("Vilken huvudstad har Sverige?")
+print("Vilken huvudstad har Norge?")
 print("1. Oslo")
 print("2. Stockholm")
 print("3. Köpenhamn")
-svar=2
+svar1=1
+svar2=2
+poäng=0
 gissning=int(input("Gissa på vilken av dessa svarsalternativ som är korrekt"))
-if svar==gissning:
+if svar1==gissning:
     print("Du svarade rätt!")
+    poäng+=1
 else:
     print("Du fick fel svar")
+
+print("Vilket år började andra världskriget")
+print("1. 1914")
+print("2. 1939")
+print("3. 1945")
+gissning=int(input("Gissa på vilken av dessa svarsalternativ som är korrekt"))
+if svar2==gissning:
+    print("Du svarade rätt!")
+    poäng+=1
+else:
+    print("Du fick fel svar")
+
+print(f"Du fick {poäng} av 2 rätt")
